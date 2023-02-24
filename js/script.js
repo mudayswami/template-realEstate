cea15d9ab16627e724bad7575f233d8b94446f69
Anubhav <121726561+mudayswami@users.noreply.github.com>
2023-02-24 19:06:08 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
